0132f0bb0956506aa2b542740a0805c55c173477
cowt1310 <tienthinh13102004@gmail.com>
2026-09-05 21:22:54 +0700

    them hello voi gioi thieu boi Thinh

---
+++ thanhvien/thinh/hello.php
@@ -0,0 +1,7 @@
+<?php
+    // Thiết lập múi giờ Việt Nam
+    date_default_timezone_set("Asia/Ho_Chi_Minh");
+
+    echo "<h1>Xin chào từ PHP!</h1>";
+    echo "<p>Hôm nay là: " . date("d/m/Y H:i:s") . "</p>";
+?>
