de4e27fb64082f9a60e124341ef0377d5972add5
HiangBao <lehoangbao9077@gmail.com>
2026-09-10 22:56:58 +0700

    delete folder sai

--- thanhvien/thinh/hello.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Thiết lập múi giờ Việt Nam
-    date_default_timezone_set("Asia/Ho_Chi_Minh");
-
-    echo "<h1>Xin chào từ PHP!</h1>";
-    echo "<p>Hôm nay là: " . date("d/m/Y H:i:s") . "</p>";
-?>
