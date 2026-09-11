0e88903776bb7fc3429e7e33759069e39854f803
cowt1310 <tienthinh13102004@gmail.com>
2026-09-11 18:22:20 +0700

    Thinh nop bai

---
+++ thanhvien/thinh/hello.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Info</title>
+</head>
+<body>
+
+<h1>Xin chào Thiết kế và Lập trình web!</h1>
+
+<p>
+    Bây giờ là <?php echo date("H:i, d/m/Y"); ?> trên server.
+</p>
+
+<?php
+phpinfo();
+?>
+
+</body>
+</html>
